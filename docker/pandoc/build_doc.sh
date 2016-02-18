6e756cabedc0a380f94426dcae5ff712ae33152e
Spoon4 <spoon@iMac-de-Spoon.local>
2016-02-18 23:07:33 +0100

    updated circle.yml file and entrypoint script for Pandoc services

--- docker/pandoc/build_doc.sh
+++ docker/pandoc/build_doc.sh
@@ -3,10 +3,17 @@
 PUID="${PUID:-0}"
 PGID="${PGID:-0}"
 
-TEMP_FILE="pandoc-temp.html"
-
+TEMP_FILE=temp.html
+# INPUT=""
+# 
+# while read line
+# do
+#     echo -e "$INPUT"
+#     $INPUT="$INPUT/source/$line "
+# done < /files.txt
+#
+# pandoc -s -f markdown_github -o $TEMP_FILE $INPUT
 pandoc -s -f markdown_github -o $TEMP_FILE /source/*.md
-
 
 pandoc -s $TEMP_FILE $*
 
